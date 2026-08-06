45f7e418ee9495effae3d902c6db04998dc1be1a
Pedro Felipe <pedrofelipinho@gmail.com>
2026-08-06 11:59:04 -0300

    Colocando  swap dos buffers para que exiba os pixels coloridos

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ int main() {
 
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
